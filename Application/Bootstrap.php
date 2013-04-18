0db00bba9db679ef63dd899a6a736de7458211f3
Spawnm <spawnm89@gmail.com>
2013-04-18 11:22:10 +0200

    up

--- Application/Bootstrap.php
+++ Application/Bootstrap.php
@@ -1,1 +1,12 @@
 <?php
+/*
+$dbc = new \Spawn\Db\Connect();
+$dbc -> connect();
+$dbc -> register();
+*/
+
+/*
+$session = new \Spawn\Session();
+$session -> validSecurity();
+$session -> register();
+*/
