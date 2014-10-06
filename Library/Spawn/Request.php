64c00c78bc08e6bb9e03901ef3a557d381cd8f3c
Spawnm <spawnm89@gmail.com>
2014-10-06 10:55:38 +0200

    up

--- Library/Spawn/Request.php
+++ Library/Spawn/Request.php
@@ -5,7 +5,7 @@
 * Request
 *
 * @author  Paweł Makowski
-* @copyright (c) 2010-2013 Paweł Makowski
+* @copyright (c) 2010-2014 Paweł Makowski
 * @license http://spawnframework.com/license New BSD License
 * @package Request
 */
--- Library/Spawn/View/Helper/DataGrid.php
+++ Library/Spawn/View/Helper/DataGrid.php
@@ -5,7 +5,7 @@
  * DataGrid
  *
  * @author  Paweł Makowski
- * @copyright (c) 2010-2013 Paweł Makowski
+ * @copyright (c) 2010-2014 Paweł Makowski
  * @license http://spawnframework.com/license New BSD License
  * @package Helper
  */
@@ -45,24 +45,20 @@ class DataGrid
     protected $_primary = 'id';
 
     /**
-     * @var string
+     * @var array
      */
     protected $_rows;
-
+    
     /**
-     * @var string
+     * @var array
      */
-    protected $_head;
+    protected $_headers;
 
     /**
      * @var string
      */
     protected $_url;
 
-    /**
-     * @var Table
-     */
-    public $table;
 
     /**
      * row to render
@@ -70,9 +66,9 @@ class DataGrid
      * @var object
      */
     public $row;
-
+    
     /**
-     * @var bool
+     * @var bool|array
      */
     protected $_search = false;
 
@@ -80,18 +76,24 @@ class DataGrid
      * @var string
      */
     protected $_name;
+    
+    /**
+    * @var integer
+    */
+    protected $_count;
 
 
     /**
      * add default row actions - [view, update, delete]
      */
     public function __construct($name = 'default')
-    {
+    {    	
         $this->_name = $name;
-        $this->_action['view'] = function($act, $id) { return '<a href="'.\Spawn\Url::site($act.'/view/'.$id).'" class="view btn btn-link">View</a>'; };
-        $this->_action['edit'] = function($act, $id) { return '<a href="'.\Spawn\Url::site($act.'/edit/'.$id).'" class="edit btn btn-link">Edit</a>'; };
-        $this->_action['delete'] = function($act, $id) { return '<a href="'.\Spawn\Url::site($act.'/delete/'.$id).'" class="delete btn btn-link">Delete</a>'; };
-        $this->table = new Table();
+        $this->_action['view'] = function($act, $id) { return '<a href="'.\Spawn\Url::site($act.'/view/'.$id).'" class="view btn btn-link"><span class="glyphicon glyphicon-search"></span></a>'; };
+        $this->_action['edit'] = function($act, $id) { return '<a href="'.\Spawn\Url::site($act.'/edit/'.$id).'" class="edit btn btn-link"><span class="glyphicon glyphicon-pencil"></span></a>'; };
+        $this->_action['delete'] = function($act, $id) { return '<a href="'.\Spawn\Url::site($act.'/delete/'.$id).'" class="delete btn btn-link"><span class="glyphicon glyphicon-trash"></span></a>'; };
+        $this->_action['up'] = function($act, $id) { return '<a href="'.\Spawn\Url::site($act.'/up/'.$id).'" class="up btn btn-link"><span class="glyphicon glyphicon-arrow-up"></span></a>'; };
+        $this->_action['down'] = function($act, $id) { return '<a href="'.\Spawn\Url::site($act.'/down/'.$id).'" class="down btn btn-link"><span class="glyphicon glyphicon-arrow-down"></span></a>'; };        
     }
 
 
@@ -116,23 +118,25 @@ class DataGrid
             if($key === $clear) {
                 $order = 'CLEAR';
             }
-
-            $a = '<a href="?'.$key.'='.$order.'">'.$val.'</a>';
-            $str .= '<th class="th_'.$key.'">'.$a.'</th>';
+			
+			$this->_headers[] = array(
+				'key'=>$key,
+				'value'=>$val,
+				'order'=>$order
+			);
         }
-        $this->_head = '<tr>'.$str.'</tr>';
 
         if(null != $search) {
-            $this->_head .= $this->getSearch($search);
+            $this->loadSearch($search);
         }
+        
         return $this;
     }
 
-    public function getSearch(array $rows)
+    public function loadSearch(array $rows)
     {
-        $this->_search = true;
+        $this->_search = array();
 
-        $str = '<tr class="tr_search">';
         $form = new \Spawn\Form();
         $request = new \Spawn\Request();
 
@@ -141,18 +145,12 @@ class DataGrid
 
             if(is_callable($row)) {
                 $data = $row();
+                $this->_search[] = array('input'=>$data);
             }else {
-                $data = $form->text($row,$request->post($row), array('class'=>'form-control'));
+            	$this->_search[] = array('row'=>$row, 'value'=>$request->post($row));		
             }
-
-            $str .= '<th class="th_'.$i.'">'.$data.'</th>';
             $i++;
         }
-        $str .= '<th>'.$form->submit('»').'</th>';
-
-        $str .= '</tr>';
-
-        return $str;
     }
     
     /**
@@ -175,6 +173,7 @@ class DataGrid
      */
     public function rows($values, $info)
     {    
+    	$this->_count = count($values);
         $rows='';
         $pri=null;
         $i=0;
@@ -186,11 +185,11 @@ class DataGrid
                 if( !is_array($key) ){
                     $row[] = \Spawn\Filter::xss($this->_getRowParam($key));
                 }elseif( isset($key[1]) && is_callable($key[1]) ){
-                    $row[] = $key[1]($this->_getRowParam($key[0]));
+                    $row[] = $key[1]($this->_getRowParam($key[0]), $data);
                 }else{
                     $str = '';
                     foreach($key as $use ){
-                        $str .= $this->_action[$use]($this->getUrl(), $pri);
+                        $str .= $this->_action[$use]($this->getUrl(), $pri, $data);
                     }
                     $row[] = $str;
                 }
@@ -199,14 +198,11 @@ class DataGrid
             $str = '';
             $j = 1;
             foreach($row as $key){
-                $str .= '<td class="td_'.$j.'">'.$key.'</td>';
+                $this->_rows[$i][$j] = $key;
                 $j++;
             }
-            $rows .= '<tr class="tr_'.$i.'">'.$str.'</tr>'.PHP_EOL;
             $i++;
         }
-
-        $this->_rows .= $rows;
         return $this;
     }
 
@@ -249,57 +245,26 @@ class DataGrid
         }
         return $this->_url;
     }
+    
+    public function count()
+    {
+    	return $this->_count;	
+    }
 
     /**
      * @return string
      */
-    public function render($class = 'table table-striped table-bordered table-hover table-condensed')
+    public function render($tpl = 'default')
     {
-        $str = $this->getTableOpen($class);
-        $str .= $this->getHead();
-        $str .= $this->getRows();
-        $str .= $this->getTableClose();
-        if($this->_search) {
-            $str = '<form action="" method="post" class="form-search">'.$str.'</form>';
-        }
+    	$di = new \Spawn\DI;
+    
+        $view = new \Spawn\View('_Datagrid/'.$tpl);
+        $view->form = $di->form;
+        $view->headers = $this->_headers;
+        $view->search = $this->_search;
+        $view->rows = $this->_rows;
+        $str = $view->render();
         return $str;
-    }
-
-    /**
-     * @param string $class
-     * @return string
-     */
-    public function getTableOpen($class = 'table table-striped table-bordered table-hover table-condensed')
-    {
-        return '<table class="'.$class.'">';
-    }
-        
-    /**
-     * @param bool $thead
-     * @return string
-     */
-    public function getHead($thead = true)
-    {
-        $str = (true == $thead)? '<thead>'.$this->_head.'</thead>'.PHP_EOL: $this->_head;
-        return $str;
-    }
-
-    /**
-     * @param bool $tbody
-     * @return string
-     */
-    public function getRows($tbody = true)
-    {
-        $str = (true == $tbody)? '<tbody>'.$this->_rows.'</tbody>'.PHP_EOL: $this->_rows;
-        return $str;
-    }
-
-    /**
-     * @return string
-     */
-    public function getTableClose()
-    {
-        return '</table>';
     }
 
     /**
